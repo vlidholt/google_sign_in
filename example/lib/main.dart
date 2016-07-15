58e17a3f16dc3527d04e0d5e366e987733d7fd93
Collin Jackson <jackson@google.com>
2016-07-15 16:13:09 -0700

    Create a template example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,85 +1,47 @@
-// Copyright 2016, the Flutter project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(
     new MaterialApp(
-      title: 'Google Sign In',
-      home: new SignInDemo()
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: new FlutterDemo()
     )
   );
 }
 
-class SignInDemo extends StatefulWidget {
+class FlutterDemo extends StatefulWidget {
+  FlutterDemo({ Key key }) : super(key: key);
+
   @override
-  State createState() => new SignInDemoState();
+  _FlutterDemoState createState() => new _FlutterDemoState();
 }
 
-class SignInDemoState extends State<SignInDemo> {
-  // Update this string with your app's client id
-  static const String _kClientId = "946753052256-j58q7kn21j491vfn5m5v56bblgcpevgg.apps.googleusercontent.com";
+class _FlutterDemoState extends State<FlutterDemo> {
+  int _counter = 0;
 
-  GoogleSignIn _gsi;
-  GoogleSignInUser _currentUser;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
-  void initState() {
-    super.initState();
-    _gsi = new GoogleSignIn(_kClientId)
-      ..onCurrentUserChanged.listen((GoogleSignInUser user) {
-        setState(() => _currentUser = user);
-      })
-      ..signInSilently();
-  }
-
-  Widget _buildBody() {
-    if (_currentUser == null) {
-      return new Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          new Text("You are not currently signed in."),
-          new RaisedButton(
-            child: new Text('SIGN IN'),
-            onPressed: _gsi.signIn
-          )
-        ]
-      );
-    } else {
-      return new Column(
-        children: <Widget>[
-          new ListItem(
-            leading: new CircleAvatar(
-              child: new ClipOval(
-                child: new NetworkImage(src: _currentUser.photoUrl)
-              )
-            ),
-            title: new Text(_currentUser.displayName),
-            subtitle: new Text(_currentUser.email)
-          ),
-          new RaisedButton(
-            child: new Text('SIGN OUT'),
-            onPressed: _gsi.disconnect
-          )
-        ],
-        mainAxisAlignment: MainAxisAlignment.spaceAround
-      );
-    }
-  }
-
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Google Sign In')
+        title: new Text('Flutter Demo')
       ),
-      body: _buildBody()
+      body: new Center(
+        child: new Text('Button tapped $_counter time${ _counter == 1 ? '' : 's' }.')
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add)
+      )
     );
   }
 }
